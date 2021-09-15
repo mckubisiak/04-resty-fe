f141fea38e2c734fba6a2c469cf255e7f862b9b1
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-14 17:46:40 -0700

    updated test, mock server, select url and change, select get radio passing

--- src/containers/Postkubi.test.jsx
+++ src/containers/Postkubi.test.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Postkubi from '../containers/Postkubi';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const url =
+  'https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51';
+
+const mockServer = setupServer(
+  rest.get(url, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          _id: '8675309',
+          name: 'Roger Dodger',
+          image: 'example.com/image.png',
+        },
+      ])
+    );
+  })
+);
 
 describe('Postkubi container', () => {
+  beforeAll(() => mockServer.listen());
+  afterAll(() => mockServer.close());
+
   it('im very unsure', async () => {
     render(<Postkubi />);
 
+    const url = screen.findByRole('textbox', { name: 'url' });
+    userEvent.type(
+      url,
+      '{selectall}{del}https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51'
+    );
+
+    const radio = await screen.findByDisplayValue('GET');
+    fireEvent.click(radio);
+    expect(radio).toBeChecked();
     
   });
-
 });
